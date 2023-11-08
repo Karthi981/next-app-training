ca2d9220f0e01c0d51ba52f554ffed5816ea8a15
Karthi981 <karkartgh@gmail.com>
2023-11-08 17:01:01 +0530

    reset password finished

--- app/ResetPassword/page.tsx
+++ app/ResetPassword/page.tsx
@@ -1,7 +1,115 @@
+"use client";
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useAtom } from "jotai";
+import { formDataAtom, profileDataAtom } from "../Store";
+import { Toaster } from "@/components/ui/toaster";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+type FormData = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  userName: string;
+};
 
 const ResetPage = () => {
-  return <div>ResetPage</div>;
+  const [details, setDetails] = useAtom(profileDataAtom);
+  const [formData, setFormData] = useAtom(formDataAtom);
+  const { toast } = useToast();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
+  const { email, firstName } = details;
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+    toast({
+      title: "Your Password is Reset" + firstName,
+      description: "Email ID:" + data.email,
+      action: <ToastAction altText="Goto schedule to undo">Ok</ToastAction>,
+    });
+    setFormData((prevData) => ({
+      ...prevData,
+      email: data.email,
+      password: data.password,
+      confirmPassword: data.confirmPassword,
+    }));
+  });
+  return (
+    <div className="min-h-screen items-start justify-center flex mt-8">
+      <form onSubmit={onSubmit}>
+        <div className="flex flex-col bg-slate-100 w-[700px] px-4 py-4 border-solid border-2 border-slate-200 ">
+          <p className="text-xl text-black underline pb-8">
+            {firstName}Do you want to reset your Password
+          </p>
+          <Label className="text-sm text-gray-400 py-2">Your Email Id is</Label>
+          <Input
+            {...register("email", {
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Email is Invalid",
+              },
+              required: "Email is required",
+            })}
+            // value={email}
+            className="py-2"
+            placeholder="Enter the Email Id"
+          />
+          <p className="text-red-400 py-2">{errors.email?.message}</p>
+          <label className="block mb-2 text-sm font-medium text-gray-400 dark:text-white">
+            Password
+          </label>
+          <input
+            {...register("password", {
+              required: "Password is required",
+              pattern: {
+                value:
+                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                message:
+                  "At least one Number,Uppercase and Special Symbol required",
+              },
+            })}
+            placeholder="••••••••"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          <p className="text-red-400 py-2">{errors.password?.message}</p>
+          <div>
+            <label className="block mb-2 text-sm font-medium  text-gray-400 dark:text-white">
+              Confirm password
+            </label>
+            <input
+              {...register("confirmPassword", {
+                required: "Password is required",
+              })}
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            <p className="text-red-400 py-2">
+              {errors.confirmPassword?.message}
+            </p>
+            <div className="py-4">
+              {" "}
+              <Button
+                disabled={!!errors.email || !!errors.userName}
+                type="submit"
+                className="px-4 py-4 w-48"
+              >
+                Reset Password
+              </Button>
+              <Toaster />
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ResetPage;
--- app/SignUp/page.tsx
+++ app/SignUp/page.tsx
@@ -89,6 +89,9 @@ const SignupPage = () => {
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
+                <p className="text-red-400 py-2">
+                  {errors.ConfirmPassword?.message}
+                </p>
                 <div>
                   <label className="block mb-2 py-2 text-sm font-medium text-green-600 dark:text-white">
                     First Name
